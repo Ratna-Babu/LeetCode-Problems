0c83525746c3f04c175541d0e0d270c7f08e85aa
Bantupalli Ratna Babu <ratna71babu@gmail.com>
2025-02-14 21:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.32%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        first, second = 1, 2 
+        for i in range(3, n + 1):
+            first, second = second, first + second  
+
+        return second
