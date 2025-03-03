c335001a9b650401c22d24571927f7242b63e189
Bantupalli Ratna Babu <ratna71babu@gmail.com>
2025-03-03 15:52:16 +0530

    Time: 7 ms (27.60%) | Memory: 12.5 MB (11.17%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -4,21 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        seen=set()
-        while(n>1):
-            if n in seen:
-                return False
+
+        seen = set()
+        while n != 1 and n not in seen:
             seen.add(n)
-            len1=len(str(n))
-            a=[]
-            while(len1>0):
-                d=10**(len1-1)
-                a.append(n//d)
-                n=n%d
-                len1=len1-1
-            sum=0
-            for i in a:
-                sum=sum+i**2
-            n=sum
-        if n == 1:
-            return True
+            n = sum(int(digit) ** 2 for digit in str(n))
+        return n == 1
