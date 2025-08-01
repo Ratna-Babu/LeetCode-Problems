c0917207015c25496628467333fc98ae34af2024
Bantupalli Ratna Babu <ratna71babu@gmail.com>
2025-08-01 19:30:59 +0530

    Time: 8 ms (50.31%) | Memory: 13.6 MB (16.48%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -4,7 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(len(nums)):
-            if sum(nums[0:i])==sum(nums[i+1:]):
+        prefix_sum = [0]*len(nums)
+        prefix_sum[0] = nums[0]
+
+        n = len(nums)
+
+        for i in range(1,n):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i]
+        
+        for i in range(n):
+            if i == 0:
+                left_sum = 0
+            else:
+                left_sum = prefix_sum[i-1]
+            right_sum = prefix_sum[n-1] - prefix_sum[i]
+            if left_sum == right_sum:
                 return i
+        
         return -1
