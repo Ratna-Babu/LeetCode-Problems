447902f3f6cdf4fa1ef45931a61ddb63f93c407e
Bantupalli Ratna Babu <ratna71babu@gmail.com>
2025-08-30 22:30:23 +0530

    Time: 7 ms (89.18%) | Memory: 17.9 MB (50.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,4 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
-        
-        freq1 = {}
-        freq2 = {}
-
-        for ch in s:
-            freq1[ch] = freq1.get(ch,0) + 1
-        for ch in t:
-            freq2[ch] = freq2.get(ch,0) + 1
-
-        return freq1 == freq2
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
